36f768b1394543bf3e0be8e773512665cc36c96c
Nicolas Girard <girard.nicolas@gmail.com>
2025-05-26 19:38:13 +0200

    Add scripts/bootstrap.sh

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+PREFIX="/usr"
+PROJNAME="taskgist"
+
+# Eget
+#   Easily install prebuilt binaries from GitHub
+#   https://github.com/zyedidia/eget
+curl https://zyedidia.github.io/eget.sh | sh && { f=eget; sudo install $f "${PREFIX}/bin" && rm $f; }
+
+# Just
+#   Just a command runner
+#   https://github.com/casey/just
+#   Config: no
+sudo eget -a x86_64 -a musl casey/just --to="${PREFIX}/bin"
+
+(cd "${PROJNAME}" && just sync && just baml-generate || exit 1)
